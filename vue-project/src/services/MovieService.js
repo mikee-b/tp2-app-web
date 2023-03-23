8108408bd1be8d06c141464b2f9743b121bf2e55
Mikee Blanchet <2136010@csfoy.ca>
2023-03-23 10:48:32 -0400

    getRecentMovies

--- vue-project/src/services/MovieService.js
+++ vue-project/src/services/MovieService.js
@@ -1,19 +1,18 @@
-//clé api : 7a43453c090876ec404f9d3117402b26
-let baseURL = 'https://api.themoviedb.org/3/';
-let headers = {
+const apiKey = '7a43453c090876ec404f9d3117402b26';
+const baseURL = 'https://api.themoviedb.org/3/';
+const headers = {
   Accept: "application/json",
   "Content-Type": "application/json"
 };
 
-const RESOURCE_NAME = "products";
 
-export async function getAllProducts() {
-  const response = await fetch(baseURL + RESOURCE_NAME, headers);
-  return await response.json();
+export async function getRecentMovies() {
+  const response = await fetch(baseURL + '/trending/movie/day', headers);
+  return await response.json().results;
 }
 
 export async function getProduct(id) {
     const response = await fetch(baseURL + RESOURCE_NAME + '/' + id, headers);
     let product = await response.json();
     return product;
-  }
+}
