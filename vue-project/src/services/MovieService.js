da2a203a4c4820a64122dec9e8eae8a57fcddb9d
Mikee Blanchet <2136010@csfoy.ca>
2023-03-27 17:27:08 -0400

    fixed api parameter in url

--- vue-project/src/services/MovieService.js
+++ vue-project/src/services/MovieService.js
@@ -1,4 +1,5 @@
 const apiKey = '7a43453c090876ec404f9d3117402b26';
+const apiKeyParam = 'api_key='
 const baseURL = 'https://api.themoviedb.org/3/';
 const headers = {
   Accept: "application/json",
@@ -7,19 +8,19 @@ const headers = {
 
 
 export async function getRecentMovies() {
-  const response = await fetch(baseURL + '/trending/movie/day' + '?' + apiKey, headers);
+  const response = await fetch(baseURL + '/trending/movie/day' + '?' + apiKeyParam + apiKey, headers);
   return await Object.keys(response.json().results).slice(0, 3);
 }
 
 export async function getMovie(id) {
-    const response = await fetch(baseURL + 'movie/' + id + '?' + apiKey, headers);
+    const response = await fetch(baseURL + 'movie/' + id + '?' + apiKeyParam + apiKey, headers);
     let movie = await response.json();
     return movie;
 }
 
 export async function searchMoviesByKeyWords(keywords) {
     // keywords format should be : "keyword1,keyword2,keyword3"
-    const response = await fetch(baseURL + 'search/movie' + '?' + 'api_key=' + apiKey + '&query=' + keywords, headers);
+    const response = await fetch(baseURL + 'search/movie' + '?' + apiKeyParam + apiKey + '&query=' + keywords, headers);
     let movies = await response.json();
     return movies;
 }
