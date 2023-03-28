4d7d87a96ee523c66d8a2ac21ccd629ce43e0be6
Mikee Blanchet <2136010@csfoy.ca>
2023-03-28 14:44:24 -0400

    fixed merge issue

--- vue-project/src/views/HomeView.vue
+++ vue-project/src/views/HomeView.vue
@@ -1,30 +1,3 @@
-
-<template>
-  <RecentMovieList :movies="movies"></RecentMovieList>
-</template>
-
-<script>
-import RecentMovieList from '../components/Home.vue'
-import { getRecentMovies } from '@/services/MovieService.js';
-
-export default {
-  components: {
-    RecentMovieList,
-  },
-  data() {
-    return {
-      movies: []
-    }
-  },
-  created() {
-    getRecentMovies().then(response => this.movies = response);
-    console.log('homeView')
-    console.log(this.movies)
-  },
-};
-</script>
-
-
 <template>
     <RecentMovieList :movies="movies"></RecentMovieList>
   </template>
