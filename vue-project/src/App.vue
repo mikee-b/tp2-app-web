d2e15f568726657daa3adf7cf22611830d8744a7
Mathys Deshaies <mathys.deshaies@icloud.com>
2023-03-28 14:25:23 -0400

    routes /movies and /movie/:id ajouté dans app.vue

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,6 +1,5 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
@@ -8,6 +7,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/movies">Movies</RouterLink>
       </nav>
   </header>
 
