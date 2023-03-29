8122af912671e2c94589c67f8619e871c56f647c
Mathys Deshaies <mathys.deshaies@icloud.com>
2023-03-29 16:29:13 -0400

    faire apparaitre trois films récents

--- vue-project/src/components/Home.vue
+++ vue-project/src/components/Home.vue
@@ -1,53 +1,49 @@
 <template>
-    <div>
-      <h2>{{ title }}</h2>
-      <ul class="movies">
-        <li
-          v-for="movie in threeMovies"
-          :key="movie.id"
-          v-bind:class="{
-            discontinued: movie.discontinued,
-            selected: selectedMovie === movie,
-          }"
-          @click="onSelect(movie)" 
-        >
-         <img src={{ movie.picture }} alt="movie picture">
-          <span class="title">{{ movie.title }}</span>
-          <!-- <span class="img">{{ movie.img }}</span>
-          <span class="release">{{ movie.release }}</span> -->
-        </li>
-      </ul>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    props: {
-      movies: {
-        type: Array,
-        default: () => [],
-      },
+  <div>
+    <h2>{{ title }}</h2>
+    <ul class="movies">
+      <li
+        v-for="movie in threeMovies"
+        :key="movie.id"
+        
+        @click="onSelect(movie)" 
+      >
+        <img :src="baseUrlImg + movie.backdrop_path" alt="movie picture">
+        <span class="title">{{ movie.title }}</span>
+        <span class="release">{{ movie.release_date }}</span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    movies: {
+      type: Array,
+      default: () => [],
     },
-    data() {
-      return {
-        title: "movies",
-        selectedMovie: null,
-        pageNumber: 1,
-      };
+  },
+  data() {
+    return {
+      title: "movies",
+      selectedmovie: null,
+      baseUrlImg: "https://image.tmdb.org/t/p/w500"
+    };
+  },
+  computed: {
+    threeMovies() {
+      return this.movies;
     },
-    computed: {
-      threeMovies() {
-        return this.movies;
-      },
+  },
+  methods: {
+    onSelect(movie) {
+      this.$router.push({ name: "movie", params: { id: movie.id } });
     },
-    methods: {
-      onSelect(movie) {
-        this.$router.push({ name: "movie", params: { id: movie.id } });
-      },
-    },
-  };
-  </script>
-  
-  <style lang="css" scoped>
-  
-  </style>
+  },
+};
+</script>
+
+<style lang="css" scoped>
+
+</style>
