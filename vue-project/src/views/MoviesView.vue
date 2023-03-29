9c3499527da7ede53e8fe8a71b1f1368891b9e65
Mathys Deshaies <mathys.deshaies@icloud.com>
2023-03-29 15:38:46 -0400

    copie d homeView pour moviesView

---
+++ vue-project/src/views/MoviesView.vue
@@ -0,0 +1,25 @@
+
+<template>
+    <RecentMovieList :movies="movies"></RecentMovieList>
+  </template>
+  
+  <script>
+  import RecentMovieList from '../components/Home.vue'
+  import { getRecentMovies } from '@/services/MovieService.js';
+  
+  export default {
+    components: {
+      RecentMovieList,
+    },
+    data() {
+      return {
+        movies: []
+      }
+    },
+    mounted() {
+      getRecentMovies().then(response => this.movies = response);
+    },
+  };
+  </script>
+  
+  
