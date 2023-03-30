a6e121f23e91bbdb3d1b48f53b924b4a8591f61d
Mathys Deshaies <mathys.deshaies@icloud.com>
2023-03-30 09:37:50 -0400

    css pour homepage update

--- vue-project/src/components/Home.vue
+++ vue-project/src/components/Home.vue
@@ -12,6 +12,7 @@
         <span class="movie-title">{{ movie.title }}</span>
         <span class="movie-release">{{ movie.release_date }}</span>
       </li>
+    
     </ul>
   </div>
 </template>
@@ -48,7 +49,7 @@ export default {
   .movies{
     display: flex;
     justify-content: space-around;
-
+    flex-flow: row wrap;
     margin: 0;
     list-style-type: none;
     padding: 0;
@@ -65,8 +66,28 @@ export default {
     width: 25vw;
   }
   .movies .movie-title{
-    font-size: 1.5rem;
+    font-size: 1.2rem;
     color: var(--main-text-color);
+    width:fit-content;
+    margin: 0.5rem 0;
+    cursor: pointer;
+    padding: 0 0.5rem;
+
+    background:
+      linear-gradient(to right, var(--second-text-color) 2px, transparent 4px) 0 0,
+      linear-gradient(to left, var(--second-text-color) 2px, transparent 4px) 100% 100%,
+      linear-gradient(to bottom, var(--second-text-color) 2px, transparent 4px) 0 0,
+      linear-gradient(to top, var(--second-text-color) 2px, transparent 4px) 100% 100%;
+
+    background-repeat: no-repeat;
+    background-size: 20px 20px;
+  }
+  .movies .movie-title:hover{
+    background:
+      linear-gradient(to right, var(--second-text-color) 2px, transparent 4px) 100% 100%,
+      linear-gradient(to left, var(--second-text-color) 2px, transparent 4px) 100% 100%,
+      linear-gradient(to bottom, var(--second-text-color) 2px, transparent 4px) 100% 100%,
+      linear-gradient(to top, var(--second-text-color) 2px, transparent 4px) 100% 100%;
   }
   .movies .movie-release{
     font-size: 1rem;
@@ -75,5 +96,11 @@ export default {
   .movies li {
     display: flex;
     flex-direction: column;
+    margin-bottom: 1rem;
+  }
+  .movies .break:nth-child(3n){
+    display: block;
+    width: 100%;
+    height: 0;
   }
 </style>
