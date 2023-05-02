1160cd50a5fa992e2e5386cddf7e5a5f5ad20108
Mathys Deshaies <mathys.deshaies@icloud.com>
2023-05-02 14:11:57 -0400

    ajouter section rating

--- src/views/MovieDetailsView.vue
+++ src/views/MovieDetailsView.vue
@@ -16,21 +16,33 @@
             <p v-if="getNumberOfStarsFromRating(movie.vote_average) <= (index - 1)" class="static-rate empty-star">★</p>
         </div>
     </div>
-    <form action="" method="post" @submit.prevent="submitForm">
-      <div class="rate">
-          <input type="radio" id="star5" name="rate" value="10" />
-          <label for="star5" title="text">5 stars</label>
-          <input type="radio" id="star4" name="rate" value="8" />
-          <label for="star4" title="text">4 stars</label>
-          <input type="radio" id="star3" name="rate" value="6" />
-          <label for="star3" title="text">3 stars</label>
-          <input type="radio" id="star2" name="rate" value="4" />
-          <label for="star2" title="text">2 stars</label>
-          <input type="radio" id="star1" name="rate" value="2" />
-          <label for="star1" title="text">1 star</label>
-      </div>
-      <button type="submit">Envoyer</button>
-    </form>
+    <!--si administrateur-->
+    <div>
+        <h3>Rating</h3>
+        <form class="rating-form">
+            <div class="rate">
+                <input type="radio" id="star5" name="rate" value="10" />
+                <label for="star5" title="text">5 stars</label>
+                <input type="radio" id="star4" name="rate" value="8" />
+                <label for="star4" title="text">4 stars</label>
+                <input type="radio" id="star3" name="rate" value="6" />
+                <label for="star3" title="text">3 stars</label>
+                <input type="radio" id="star2" name="rate" value="4" />
+                <label for="star2" title="text">2 stars</label>
+                <input type="radio" id="star1" name="rate" value="2" />
+                <label for="star1" title="text">1 star</label>
+            </div>
+            <div>
+                <label for="text">Message : </label>
+                <textarea type="text" placeholder="Commentaire ici..." maxlength="255"></textarea>
+            </div>
+            <!--si aucun commentaire-->
+            <button id="add-rating" type="submit">Ajouter</button>
+            <!--si commentaire éxiste déjà-->
+            <button id="update-rating" type="submit">Modifier</button>
+        </form>
+    </div>
+
   </template>
   
   <script>
@@ -70,12 +82,114 @@
       },
       getNumberOfStarsFromRating(num) {
         return Math.round(num / 2);
+      },
+      openSucessPopUp(){
+        document.getElementById("myModal").style.display = "block";
+      },
+      closeSucessPopUp(){
+        document.getElementById("myModal").style.display = "none";
       }
     }
   };
   </script>
   
   <style lang="css" scoped>
+.card_commentary{
+    width: 30%;
+    aspect-ratio: 3/2;
+    border-radius: 5px;
+    border: 4px solid var(--second-text-color);
+}
+#myBtn {
+  background-color: #4CAF50;
+  border: none;
+  color: white;
+  padding: 10px 20px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 20px;
+  cursor: pointer;
+}
+/* The Modal (background) */
+.modal {
+  display: none; /* Hidden by default */
+  position: fixed; /* Stay in place */
+  z-index: 1; /* Sit on top */
+  left: 0;
+  top: 0;
+  width: 100%; /* Full width */
+  height: 100%; /* Full height */
+  overflow: auto; /* Enable scroll if needed */
+  background-color: rgb(0,0,0); /* Fallback color */
+  background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
+}
+
+/* Modal Content/Box */
+.modal-content {
+  background-color: #fefefe;
+  margin: 15% auto; /* 15% from the top and centered */
+  padding: 20px;
+  border: 1px solid #888;
+  width: 80%; /* Could be more or less, depending on screen size */
+}
+
+/* The Close Button */
+.close {
+  color: #aaaaaa;
+  float: right;
+  font-size: 28px;
+  font-weight: bold;
+}
+
+.close:hover,
+.close:focus {
+  color: #000;
+  text-decoration: none;
+  cursor: pointer;
+}
+
+
+
+
+h3{
+    margin-top: 1.5rem;
+    color: var(--main-text-color);
+    text-align: center;
+}
+.rating-form{
+    display: block;
+    margin: 1rem auto 5rem auto;
+    width:50%;
+    padding: 1rem;
+    border: 3px solid var(--second-text-color);
+    border-radius: 10px;
+}
+
+.rating-form label{
+    color: var(--main-text-color);
+    font-size: 1.4rem;
+}
+
+.rating-form textarea{
+    width:100%;
+    height: 5.2rem;
+    resize: none;
+    font-size: 1rem;
+    border: 3px solid var(--second-text-color);
+    border-radius: 10px;
+}
+
+.rating-form > div {
+    margin: 0.4rem;
+}
+
+.rating-form button {
+    margin: 0.4rem;
+    margin-top: 2rem;
+}
+
 p{
     color: var(--second-text-color);
 }
@@ -106,7 +220,8 @@ img{
 }
 /*css étoiles*/
 .rate {
-    float: left;
+    display: block;
+    margin: 0 auto;
     height: 46px;
     padding: 0;
 }
